0ea3742c9184feeb35a3f20a481b3548007f501e
aishahsa <aishahsa@gmail.com>
2020-02-28 18:12:01 -0500

    ?

---
+++ clock/script.js
@@ -0,0 +1,8 @@
+var f1p1 = document.getElementById("f1p1");
+console.log(f1p1)
+
+
+setTimeout(function(){
+	f1p1.style.border = "";
+
+}, 1000);
---
+++ plant/script1.js
@@ -0,0 +1,72 @@
+console.log("hello!");
+
+
+
+// var one = document.getElementById("f1p1");console.log("one")
+// var two = document.getElementById("f1p2");console.log("two")
+// var three = document.getElementById("f1p3");console.log("three")
+// var four = document.getElementById("f1p4");console.log("four")
+
+// var five = document.getElementById("f2p1");console.log("five")
+// var six = document.getElementById("f2p2");console.log("six")
+// var seven = document.getElementById("f2p3");console.log("seven")
+// var eight = document.getElementById("f2p4");console.log("eight")
+
+
+setTimeout(function(){
+
+	f1p1.style.opacity = "1";
+	f1p2.style.opacity = "1";
+	f1p3.style.opacity = "1";
+	f1p4.style.opacity = "1";
+
+}, 3000);
+
+
+setTimeout(function(){
+
+	f2p1.style.opacity = "1";
+	f2p2.style.opacity = "1";
+	f2p3.style.opacity = "1";
+	f2p4.style.opacity = "1";
+
+}, 6000);
+
+setTimeout(function(){
+
+	f3p1.style.opacity = "1";
+	f3p2.style.opacity = "1";
+	f3p3.style.opacity = "1";
+	f3p4.style.opacity = "1";
+
+}, 9000);
+
+setTimeout(function(){
+
+	f4p1.style.opacity = "1";
+	f4p2.style.opacity = "1";
+	f4p3.style.opacity = "1";
+	f4p4.style.opacity = "1";
+
+}, 12000);
+
+setTimeout(function(){
+
+	f5p1.style.opacity = "1";
+	f5p2.style.opacity = "1";
+	f5p3.style.opacity = "1";
+	f5p4.style.opacity = "1";
+
+}, 15000);
+
+setTimeout(function(){
+	// f1p1.style.border = "red"
+	f1p1.style.rotate = "360deg"
+
+}, 17000)
+
+	console.log("rot")
+
+
+
+
