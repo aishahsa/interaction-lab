c85c5e781f5da4f9e3428af7f7484ec1193f9b03
aishahsa <aishahsa@gmail.com>
2020-03-06 17:16:42 -0500

    I dont even know why tho

---
+++ clock/script.js
@@ -0,0 +1,18 @@
+var hour;
+var min;
+
+
+function timeInterpret(){
+
+  var hour = (myDate.getHours);
+  var min = (myDate.getMinutes);
+
+  if(hour = 0){
+    $('.hour').append('0')
+  }
+
+
+}
+console.log(hour)
+
+timeInterpret
