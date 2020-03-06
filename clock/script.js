6860a79be0741ee29155d657c0e7eddf0bc63f7f
aishahsa <aishahsa@gmail.com>
2020-03-06 17:55:51 -0500

    woooho

--- clock/script.js
+++ clock/script.js
@@ -4,15 +4,95 @@ var min;
 
 function timeInterpret(){
 
-  var hour = (myDate.getHours);
-  var min = (myDate.getMinutes);
+  var myDate = new Date;
 
-  if(hour = 0){
-    $('.hour').append('0')
+  hour = myDate.getHours();
+  min = (myDate.getMinutes);
+
+  if(hour == 0){
+    $('.words').html('sleep')
+  }
+  if(hour == 1){
+    $('.words').html('sleep')
+  }
+  if(hour == 2){
+    $('.words').append('sleep')
+  }
+  if(hour == 3){
+    $('.words').append('sleep')
+  }
+  if(hour == 4){
+    $('.words').append('sleep')
+  }
+  if(hour == 5){
+    $('.words').append('sleep')
+  }
+  if(hour == 6){
+    $('.words').append('sleep')
+  }
+  if(hour == 7){
+    $('.words').append('sleep')
+  }
+  if(hour == 8){
+    $('.words').append('get dressed')
+  }
+  if(hour == 9){
+    $('.words').append('class')
+  }
+  if(hour == 10){
+    $('.words').append('class')
+  }
+  if(hour == 11){
+    $('.words').append('class')
+  }
+  if(hour == 12){
+    $('.words').append('eat')
+  }
+  if(hour == 13){
+    $('.words').append('work')
+  }
+  if(hour == 14){
+    $('.words').append('work')
+  }
+  if(hour == 15){
+    $('.words').append('run')
+  }
+  if(hour == 15){
+    $('.words').append('run')
+  }
+  if(hour == 16){
+    $('.words').append('eat')
+  }
+  if(hour == 17){
+    $('.words').append('work')
+  }
+  if(hour == 18){
+    $('.words').append('work')
+  }
+  if(hour == 19){
+    $('.words').append('eat')
+  }
+  if(hour == 20){
+    $('.words').append('other')
+  }
+  if(hour == 21){
+    $('.word').append('other')
+  }
+  if(hour == 22){
+    $('.words').append('other')
+  }
+  if(hour == 23){
+    $('.words').append('other')
+  }
+  if(hour == 00){
+    $('.words').append('sleep')
   }
 
 
-}
-console.log(hour)
 
-timeInterpret
+}
+
+timeInterpret()
+console.log(hour)
+console.log("woo")
+setInterval(timeInterpret, 10000)
